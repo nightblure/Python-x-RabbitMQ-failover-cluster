e21aa271fd7c7f93880bab0d65b491cdd25ed6e8
night <vanobel159@gmail.com>
2023-01-24 21:11:08 +0300

    continue fix project idea for working load balancer

--- src/producer.py
+++ src/producer.py
@@ -3,7 +3,7 @@ import time
 
 import pika
 
-from src.settings import FIRST_NODE_URL, SECOND_NODE_URL
+from src.settings import FIRST_NODE_URL
 from src.utils import get_broker_connection, send_message
 
 """
@@ -27,22 +27,18 @@ from src.utils import get_broker_connection, send_message
 
 
 def main():
-    first_channel = get_broker_connection(FIRST_NODE_URL).channel()
-    second_channel = get_broker_connection(SECOND_NODE_URL).channel()
+    channel = get_broker_connection(FIRST_NODE_URL).channel()
 
-    credentials = pika.PlainCredentials('admin', 'admin')
-    parameters = pika.ConnectionParameters(host='localhost', port=8000, credentials=credentials)
-    connection = pika.BlockingConnection(parameters)
+    # credentials = pika.PlainCredentials('admin', 'admin')
+    # parameters = pika.ConnectionParameters(host='rabbit_node_1', port=5670, credentials=credentials)
+    # connection = pika.BlockingConnection(parameters)
 
     for i in range(1, 6):
         body = f'test msg №{i}'
-
-        send_message(first_channel, body, 'ex1', 'key')
+        send_message(channel, body, 'ex1', 'key')
         time.sleep(1)
-        send_message(second_channel, body, 'ex2', 'key')
 
-    first_channel.close()
-    second_channel.close()
+    channel.close()
 
 
 if __name__ == '__main__':
