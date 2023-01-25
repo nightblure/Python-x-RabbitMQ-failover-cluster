fca40cde561f8752731db8a54a1963936023cf24
night <vanobel159@gmail.com>
2023-01-25 17:38:29 +0300

    fix entrypoint and docker-compose for autoclustering after

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-
-#set -e
-set -euo pipefail
-
+set -e
 chmod 400 /var/lib/rabbitmq/.erlang.cookie
+
+HOSTNAME=`env hostname`
+
+/usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
+sleep 5
+rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
+rabbitmqctl stop_app
+rabbitmqctl join_cluster rabbit@rabbit_node_1
+rabbitmqctl start_app
+
+tail -f /var/log/rabbitmq/*.log
